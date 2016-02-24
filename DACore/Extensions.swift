7b5a579a225ab5dae31cc3eda81ca7021c956c30
Chris Mullins <cmullins@dragonarmy.com>
2016-02-24 14:21:33 -0500

    Adds NSDate to extensions to make it Comparable

--- DACore/Extensions.swift
+++ DACore/Extensions.swift
@@ -538,3 +538,13 @@ extension SKSpriteNode
         }
     }
 }
+
+public func ==(lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs === rhs || lhs.compare(rhs) == .OrderedSame
+}
+
+public func <(lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs.compare(rhs) == .OrderedAscending
+}
+
+extension NSDate: Comparable { }
